057e5a8efccff0a06a765622bcec68efe38d4c62
vin0325 <vincent900325@gmail.com>
2021-08-26 16:44:15 +0800

    for lesson one git practice add hello_world

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/vincent/test/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/vincent/test/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/vincent/test/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/vincent/test/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/vincent/test/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/vincent/test/src:$ROS_PACKAGE_PATH"
