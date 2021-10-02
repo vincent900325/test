416f493d990689277b38e15336bd2923b27f3274
vincent900325 <vincent900325@gmail.com>
2021-10-02 17:17:30 +0800

    lesson_ros_class publisher subscribe

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -11,5 +11,6 @@ export CMAKE_PREFIX_PATH="/home/vincent/test/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/vincent/test/devel/lib:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="/home/vincent/test/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/vincent/test/build'
+export PYTHONPATH="/home/vincent/test/devel/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES='/home/vincent/test/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/vincent/test/src:$ROS_PACKAGE_PATH"
